7bc47bf484f64ff5feed4a72ee1ebb4bf25d7ffe
geraldo <geeferreirasd034@gmail.com>
2021-03-31 14:47:58 -0300

    creating the poll template

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
+class Survey(models.Model):
+    sex_options = (
+        ('F', 'FEMININO'),
+        ('M', 'MASCULINO'),
+        ('O', 'OUTROS')
+    )
+    
+    language_settings  = (
+        ('JS', 'JAVASCRIPT'),
+        ('PY', 'PYTHON'),
+        ('TY', 'TYPESCRIPT'),
+        ('JV', 'JAVA'),
+        ('C#', 'C#'),
+        ('RB', 'BUBY')
+    )
+    
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    sex = models.CharField(
+        max_length=1, choices=sex_options, blank=False, null=False)
+    language = models.CharField(
+        max_length=2, choices=language_settings, blank=False, null=False)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    begin_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.full_name
--- core/views.py
+++ core/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        return 'save'
     return render(request, 'core/home.html')
