fc6b244d878ab7422a36b1dd66e2083d2bd0b644
geraldo <geeferreirasd034@gmail.com>
2021-04-01 08:59:37 -0300

    method of saving a survey with your ratings

--- core/forms.py
+++ core/forms.py
@@ -6,8 +6,6 @@ class SurveyForm(forms.ModelForm):
     full_name = forms.CharField(label='campo full name obrigatorio', required=True)
     email = forms.EmailField(label='campo E-mail obrigatorio', required=True)
     phone = forms.CharField(label='campo phone obrigatorio', required=True)
-    sex = forms.CharField(label='campo sex obrigatorio', required=True)
-    language = forms.CharField(label='campo language obrigatorio', required=True)
     description = forms.CharField(label='campo description obrigatorio', required=True)
     
     class Meta:
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SurveyForm
+from .models import Survey
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        return 'save'
-    return render(request, 'core/home.html')
+        form = SurveyForm(request.POST)
+        if form.is_valid():
+            # new_survey = form.
+            form.save()
+        return redirect('home')
+    forms = SurveyForm()
+    return render(request, 'core/home.html', {'forms':forms})
