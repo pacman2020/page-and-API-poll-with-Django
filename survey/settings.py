a120dbc5bd69ad8e7b0ff6cdaafd029d9a62afe1
geraldo <geeferreirasd034@gmail.com>
2021-04-05 16:29:02 -0300

    using Django login and logout system

--- survey/settings.py
+++ survey/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
