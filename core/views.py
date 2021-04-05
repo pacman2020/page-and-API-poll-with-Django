2ef7bb9cb281577d2236938f339488584dd37add
geraldo <geeferreirasd034@gmail.com>
2021-04-05 19:51:55 -0300

    personalized messages of success or alert with error handling if there is no survey

--- core/views.py
+++ core/views.py
@@ -1,18 +1,22 @@
-from django.core import paginator
+from django.contrib.messages.api import error
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SurveyForm
 from .models import Survey
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-#filros survey
-#priva rotas deletar, update, list, detail
+#aletar de erros
+#message personalizadas
+#criar api de apenas par lista todas pesquisa
+#estilzar tudo kkk
 
 def home(request):
     if request.method == 'POST':
         form = SurveyForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'salvo com sucesso!')
         return redirect('home')
     forms = SurveyForm()
     return render(request, 'core/home.html', {'forms':forms})
@@ -72,31 +76,40 @@ def list_survey(request):
 
 @login_required
 def deyail_survey(request, pk):
-    survey = get_object_or_404(Survey,pk=pk)
-    
-    return render(
-        request, 'core/detail-survey.html', 
-        {'survey':survey })
+    try:
+        survey = Survey.objects.get(pk=pk)
+        return render( request, 'core/detail-survey.html', {'survey':survey })
+    except :
+        messages.warning(request, 'pesquisa não encontrada!')
+        return redirect('home')
  
 @login_required    
 def update_survey(request, pk):
-    survey = get_object_or_404(Survey,pk=pk)
-    
-    if request.method == 'POST':
-        form = SurveyForm(request.POST, instance=survey)
-        if form.is_valid():
-            # new_survey = form.
-            form.save()
-        return redirect('detail-survey',  pk=survey.pk)
-    
-    forms = SurveyForm(instance=survey)
-    return render(request, 'core/update-survey.html', {'forms':forms})
+    try:
+        survey = get_object_or_404(Survey,pk=pk)
+        
+        if request.method == 'POST':
+            form = SurveyForm(request.POST, instance=survey)
+            if form.is_valid():
+                # new_survey = form.
+                form.save()
+            return redirect('detail-survey',  pk=survey.pk)
+        
+        forms = SurveyForm(instance=survey)
+        return render(request, 'core/update-survey.html', {'forms':forms})
+    except :
+        messages.warning(request, 'pesquisa não encontrada!')
+        return redirect('home')
 
 @login_required
 def delete_survey(request, pk):
-    survey = get_object_or_404(Survey,pk=pk)
-    
-    if survey:
-        survey.delete()
-        return redirect('list-surveys')
-    return redirect('detail-survey',  pk=survey.pk)
+    try:
+        survey = Survey.objects.get(Survey,pk=pk)
+        
+        if survey:
+            survey.delete()
+            return redirect('list-surveys')
+        return redirect('detail-survey',  pk=survey.pk)
+    except:
+        messages.warning(request, 'pesquisa não encontrada!')
+        return redirect('home')
